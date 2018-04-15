8736d8077e24cad0470df9808b855e7344c7ee75
FrancoPalau <palaufranco12@gmail.com>
2018-04-15 12:34:47 -0300

    mutacion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def creacion_ordenes(cant_tipos_produc, cant_ordenes, min_size, max_size):
     lista_ordenes = []  # Lista donde se almacenan todas las ordenes
@@ -45,7 +46,7 @@ def fitness_interno(lista_ordenes, almacen_actual):
             i[pasadas + 1], i[pos_min + pasadas + 1] = (i[pos_min + pasadas + 1], i[pasadas + 1]) #Intercambiamos posiciones
 
     promedio_dist = dist_total / len(lista_ordenes)
-    print(np_mapa)
+    # print(np_mapa)
     return promedio_dist
 
 def seleccion_padres(fitness_actual, poblacion_actual):
@@ -56,9 +57,10 @@ def seleccion_padres(fitness_actual, poblacion_actual):
     poblacion_actual[int((len(fitness_actual) / 2)):] = poblacion_actual[:int((len(fitness_actual) / 2))]
 
 def cruzamiento(poblacion_actual):
-    p_cruce_1 = np.random.randint(0,len(poblacion_actual[0])-1)
-    p_cruce_2 = np.random.randint(p_cruce_1+1,len(poblacion_actual[0]))
+
     for i in range(0,len(poblacion_actual),2):
+        p_cruce_1 = np.random.randint(0, len(poblacion_actual[0]) - 1)
+        p_cruce_2 = np.random.randint(p_cruce_1 + 1, len(poblacion_actual[0]))
         aux1 = poblacion_actual[i][p_cruce_1:p_cruce_2]
         aux2 = poblacion_actual[i+1][p_cruce_1:p_cruce_2]
         aux_ord_1 = np.concatenate([poblacion_actual[i][p_cruce_2:],poblacion_actual[i][:p_cruce_2]])
@@ -73,38 +75,60 @@ def cruzamiento(poblacion_actual):
                 aux1 = np.append(aux1, [gen])
         poblacion_actual[i+1] = np.concatenate([aux1[dif:], aux1[:dif]])
 
+def mutacion(poblacion_actual):
+    for i in range(0, len(poblacion_actual)):
+        gen_1 = np.random.randint(0, len(poblacion_actual[0]))
+        gen_2 = np.random.randint(0, len(poblacion_actual[0]))
+        poblacion_actual[i][gen_1], poblacion_actual[i][gen_2] = (poblacion_actual[i][gen_2], poblacion_actual[i][gen_1])
+
 
 if __name__ == "__main__" :
 
+    fig = plt.figure()
+
     #----Creacion de las Ordenes----#
     np.random.seed(101) #Para que siempre trabajemos con las mismas ordenes
-    lista_ordenes = creacion_ordenes(16,2,4,10)
+    lista_ordenes = creacion_ordenes(16,20,4,10)
 
     #----Creacion de la poblacion inicial----#
     ejemplo_almacen = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-    poblacion_actual = creacion_poblacion(ejemplo_almacen,10)
+    poblacion_actual = creacion_poblacion(ejemplo_almacen,100)
 
     #----Calculo del fitness de la poblacion inicial----#
     fitness_actual = []
     for individuo in poblacion_actual:
         fitness_actual.append(fitness_interno(lista_ordenes.copy(),individuo))
-    fitness_total = sum(fitness_actual)
+    fitness_prom = sum(fitness_actual)/len(poblacion_actual)
     print(fitness_actual)
+    print(fitness_prom)
 
     #----Loop del algo genetico----#
+    vec_fit_prom = []
+    vec_fit_prom.append(fitness_prom)
+    vec_it = []
     it = 0
-    while(it == 0):
+    vec_it.append(it)
+    while(it < 1000):
 
         #----Seleccion Padres----#
         seleccion_padres(fitness_actual,poblacion_actual)
-        print("Vieja",poblacion_actual)
+        # print("Vieja",poblacion_actual)
         #----Evolucion----#
         cruzamiento(poblacion_actual)
-        print("Nueva",poblacion_actual)
-
+        mutacion(poblacion_actual)
+        # print("Nueva",poblacion_actual)
         #----Calculo fitness----#
-
-
+        fitness_actual = []
+        for individuo in poblacion_actual:
+            fitness_actual.append(fitness_interno(lista_ordenes.copy(), individuo))
+        fitness_prom = sum(fitness_actual) / len(poblacion_actual)
+        print(fitness_actual)
+        print(fitness_prom)
+        vec_fit_prom.append(fitness_prom)
         it+=1
+        vec_it.append(it)
+
+    plt.plot(vec_it,vec_fit_prom)
+    plt.show()
 
 
